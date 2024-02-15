74b40ddcaf86b280007fe3133f04c17465eb87c1
Victor Ramirez <vramirezgarcia64@correo.unicordoba.edu.co>
2024-02-14 21:13:54 -0500

    add security config

--- src/main/java/com/test/api/marvelapi/persistence/entity/User.java
+++ src/main/java/com/test/api/marvelapi/persistence/entity/User.java
@@ -2,9 +2,18 @@ package com.test.api.marvelapi.persistence.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -35,8 +44,46 @@ public class User {
         return username;
     }
 
+    @Override
+    public boolean isAccountNonExpired() {
+        return !accountExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !accountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !credentialsExpired;
+    }
+
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        if(role == null){
+            return null;
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.getAuthority()));
+
+        if(role.getPermissions() == null){
+            return authorities;
+        }
+
+        role.getPermissions().stream()
+                .forEach(each -> {
+                    String permissionName = each.getPermission().getName();
+                    authorities.add(new SimpleGrantedAuthority(permissionName));
+                });
+
+        return authorities;
     }
 
     public String getPassword() {
